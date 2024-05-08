0c351da88133b77d23e9d83c811a945ed152b709
yussufhh <0793477632g@gmail.com>
2024-05-08 17:16:07 +0300

    complete assignment on simple cal.in js

---
+++ index.js
@@ -0,0 +1,36 @@
+function add() {
+    // we'll fill this in shortly
+  }
+    function add(a,b) {
+        return  a+b;
+      }
+  
+  function subtract(a,b) {
+    // we'll fill this in shortly
+    return a-b;
+  }
+  function multiply(a,b) {
+    // we'll fill this in shortly
+    return  a*b;
+  }
+  function divide(a,b) {
+    // we'll fill this in shortly
+    return  a/b;
+  }
+  function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+  return n - 1;
+}
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
+
+
+
+  
